7a7cbffe73d0d7f7068a1d8c1f41397e085b8466
Catst <scox@centralmethodist.edu>
2023-10-23 16:22:10 -0500

    base flashcards

---
+++ INSTALL.py
@@ -0,0 +1,13 @@
+import os
+os.system('cmd /c "pip install xlrd"')
+os.system('cmd /c "pip install Cython"')
+os.system('cmd /c "pip install pandas"')
+os.system('cmd /c "pip install openpyxl"')
+os.system('cmd /c "pip install tk"')
+os.system('cmd /c "pip install pygubu-designer"')
+print("you no longer need the INSTALL file. Add terms/defitions you like to the database file, then use flashcards to execute the program")
+
+#unused content
+#os.system('cmd /c "pip install playsound"')
+#os.system('cmd /c "pip install bindglobal"')
+#os.system('cmd /c "pip install debugpy"')
---
+++ flashcards.py
@@ -0,0 +1,175 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import pandas as pd
+import random as ra
+class cards:
+    def __init__(self):
+        self.remove = []
+        self.data = pd.read_excel('database.xlsx')
+        self.TermsLeft = self.data['TERMS'].values.tolist()        
+        self.POS = ra.randrange(0,len(self.data))
+        self.card = self.data.iloc[self.POS]
+        self.cardTerm = str(self.card.loc["TERMS"])
+        self.cardDef = str(self.card.loc["DEF"])
+        self.buttons = True
+        self.debug1 = input("debug on or off")
+        self.debug = self.debug1.upper()
+    def rerun(self,removeCard = False):
+        if self.debug == "ON":
+            print("current position")
+            print(self.POS)
+            print("the current term is")
+            print(self.cardTerm)
+            print("and the answer is")
+            print(self.cardDef)
+            print("currently in remove")
+            print(self.remove)
+        if removeCard == True:
+            self.remove.append(self.POS)
+            if self.debug == "ON":
+                print("this position shouldn't show up again")
+                print(self.POS)
+        self.lastPOS = self.POS
+        self.POS = ra.randrange(0,len(self.TermsLeft))
+        if self.debug == "Y":
+            print("new position #1")
+            print(self.POS)
+        if self.POS in self.remove:
+            if self.debug == "Y":
+                print("current position is in remove. will pick a different number")
+            while self.POS in self.remove:
+                self.POS = ra.randrange(0,len(self.TermsLeft))
+                if len(self.remove) >= len(self.TermsLeft):
+                    if self.debug == "Y":
+                        print("all positions are in remove")
+                    self.buttons = False
+                    break
+        if self.POS == self.lastPOS:
+            if self.debug == "Y":
+                print("the new position can't be the same as the last.will pick a different number")
+            self.rerun()
+            pass
+        self.card = self.data.iloc[self.POS]
+        self.cardTerm = str(self.card.loc["TERMS"])
+        self.cardDef = str(self.card.loc["DEF"])
+        if self.debug == "Y":
+            print("new position. final number")
+            print(self.POS)
+            print("the term is")
+            print(self.cardTerm)
+            print("and the answer is")
+            print(self.cardDef)
+            print("END OF CARDS")
+            print("________________________")
+        pass
+    
+    pass
+class FlashcardsUiApp:
+    def __init__(self, master = None):
+        # from cards
+        self.c = cards()
+        self.cardText = self.c.cardTerm
+        #-----
+        self.flashcards = tk.Tk() if master is None else tk.Toplevel(master)
+        self.flashcards.configure(
+            height=200,
+            relief="flat",
+            takefocus=True,
+            width=200)
+        #unused code ;(
+        '''
+        self.flashcards.bind('<Left>', self.left_key)
+        self.flashcards.bind('<Right>', self.right_key)
+        self.flashcards.bind('<KeyRelease-Down>',self.down_key)
+        '''
+        self.knowButton = ttk.Button(self.flashcards)
+        self.knowButton.configure(text='know', width=50)
+        self.knowButton.grid(column=0, ipady=20, row=1, sticky="w")
+        self.knowButton.configure(command=self.KnowBu)
+        self.notKnowButton = ttk.Button(self.flashcards)
+        self.notKnowButton.configure(text="don't know", width=50)
+        self.notKnowButton.grid(column=0, ipady=20, row=1, sticky="e")
+        self.notKnowButton.configure(command=self.notKnowBu)
+        self.filpCardButton = ttk.Button(self.flashcards)
+        self.filpCardButton.configure(text='filp card')
+        self.filpCardButton.grid(column=0, ipady=20, row=1, sticky="n")
+        self.filpCardButton.configure(command=self.filpCardBu)
+        
+        self.viewedCard = tk.Text(self.flashcards)
+        self.viewedCard.configure(
+            font="{Arial} 20 {}",
+            height=10,
+            relief="flat",
+            takefocus=False,
+            width=50,)
+        self.viewedCard.insert("1.0", self.cardText)
+        self.viewedCard.grid(column=0, row=0)
+
+
+        # Main widget
+        self.mainwindow = self.flashcards
+        self.viewedCard.configure(state = 'disabled')
+
+    def run(self):
+        self.mainwindow.mainloop()
+    def setText(self,Text):
+        self.viewedCard.delete('1.0', '10000000000.0')
+        self.viewedCard.insert("1.0", Text)
+        pass
+    def KnowBu(self):
+        if self.c.buttons == False:
+            self.viewedCard.configure(state = 'normal')
+            self.setText("All cards have been studied")
+            self.filpCardButton["state"] = "disabled"
+            self.notKnowButton["state"] = "disabled"
+            self.knowButton["state"] = "disabled"
+            self.viewedCard.configure(state = 'disabled')
+        else:
+            self.viewedCard.configure(state = 'normal')
+            self.c.rerun(removeCard = True)
+            self.setText(self.c.cardTerm)
+            self.viewedCard.configure(state = 'disabled')
+            pass
+
+
+    def notKnowBu(self):
+        self.viewedCard.configure(state = 'normal')
+        self.c.rerun(removeCard = False)
+        self.setText(self.c.cardTerm)
+        self.viewedCard.configure(state = 'disabled')
+        pass
+
+    def filpCardBu(self):
+        self.viewedCard.configure(state = 'normal')
+        if self.cardText == self.c.cardTerm:
+            self.cardText = self.c.cardDef
+            self.setText(self.cardText)
+            self.viewedCard.configure(state = 'disabled')
+            pass
+        else:
+            self.cardText = self.c.cardTerm
+            self.setText(self.cardText)
+            self.viewedCard.configure(state = 'disabled')
+            pass
+        self.viewedCard.configure(state = 'disabled')
+        pass
+    #unused code ;(
+    '''
+    @staticmethod
+    def left_key(self,event):
+        self.notKnowBu()
+        pass
+    def right_key(self,event):
+        self.KnowBu()
+        pass
+    def down_key(self,event):
+        self.filpCardBu()
+        pass
+    '''
+
+if __name__ == "__main__":
+    app = FlashcardsUiApp()
+    app.run()
+
+
+
